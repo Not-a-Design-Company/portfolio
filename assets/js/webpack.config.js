534aaf8ed8b806d5f88b3b1031916c586feb9fa6
Tommaso Montanari <73716933+montax@users.noreply.github.com>
2023-12-13 11:36:29 +0100

    Portfolio V1.0

---
+++ assets/js/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.bundle.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
